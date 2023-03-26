20465d150a65c59b3f030a8f49aca321546f7136
Ryan Camp <campryan@comcast.net>
2023-03-26 12:26:46 -0600

    Strip styling and content from App and add homepage component

--- world_synthesis_fe/src/App.tsx
+++ world_synthesis_fe/src/App.tsx
@@ -1,31 +1,9 @@
-import { useState } from 'react'
-import heartImage from './assets/red_heart.gif'
+import Homepage from "pages/Homepage";
 
 const App = () => {
-  const [answer, setAnswer] = useState("");
-
-  const handleButtonClick = () => {
-    setAnswer("To the end of the cul-de-sac.")
-  }
 
   return (
-    <div className="App">
-      <h1>World Synthesis</h1>
-      <h2>Synthesizing a better world through collaboration</h2>
-
-      <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
-        <h3>How much does Ryan</h3>
-        <img src={heartImage} className="heart" alt="heart love" />
-        <h3>Michelle?</h3>
-      </div>
-
-      <div className="card">
-        <button style={{border: '1px solid black'}}onClick={handleButtonClick}>
-          Click me to find out
-        </button>
-      </div>
-      <h1>{answer}</h1>
-    </div>
+    <Homepage />
   )
 }
 
