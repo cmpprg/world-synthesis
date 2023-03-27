3fb64b2bb275d66b4ace9bb1d646b56983c2fb8e
Ryan Camp <campryan@comcast.net>
2023-03-26 18:11:40 -0600

    Fix linting errors in App component

--- world_synthesis_fe/src/App.tsx
+++ world_synthesis_fe/src/App.tsx
@@ -1,13 +1,12 @@
-import { CssBaseline } from '@mui/material';
+import { CssBaseline } from "@mui/material";
 import Homepage from "pages/Homepage";
 
 const App = () => {
-
   return (
     <CssBaseline>
       <Homepage />
     </CssBaseline>
-  )
-}
+  );
+};
 
-export default App
+export default App;
