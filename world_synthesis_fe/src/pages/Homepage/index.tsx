c3bd5aad9625e916e940d6dae0b289f97f30b977
Ryan Camp <campryan@comcast.net>
2023-03-26 18:43:49 -0600

    Add some basic layout and content to Homepage

--- world_synthesis_fe/src/pages/Homepage/index.tsx
+++ world_synthesis_fe/src/pages/Homepage/index.tsx
@@ -1,13 +1,44 @@
 import spinningGlobe from "assets/spinning_globe_earth_regular.gif";
+import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
 
-const Homepage = () => {
-  return (
-    <div>
-      <img src={spinningGlobe} className="spinning globe" alt="spinning globe earth" />
-      <h1>World Synthesis</h1>
-      <h2>Synthesizing a better world through collaboration</h2>
-    </div>
-  )
-}
+const Homepage = () => (
+  <Container
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      height: "70%",
+    }}
+  >
+    <Box>
+      <img
+        src={spinningGlobe}
+        className="spinning globe"
+        alt="spinning globe earth"
+      />
+    </Box>
+    <Typography variant="h1">World Synthesis</Typography>
+    <Typography variant="h3">
+      Synthesizing a Better World Through Collaboration
+    </Typography>
+    <ButtonGroup
+      fullWidth
+      sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        paddingTop: "2rem",
+      }}
+    >
+      <Button variant="contained" color="primary" sx={{ width: "20%" }}>
+        Mission
+      </Button>
+      <Button variant="contained" color="secondary" sx={{ width: "20%" }}>
+        Rules and Regulations
+      </Button>
+    </ButtonGroup>
+  </Container>
+);
 
 export default Homepage;
