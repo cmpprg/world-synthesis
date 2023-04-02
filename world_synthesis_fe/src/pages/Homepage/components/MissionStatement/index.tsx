66cfa6dfb85444df057278f14c0a90c7fc18ad9c
Ryan Camp <campryan@comcast.net>
2023-04-02 14:50:41 -0600

    Add to mission statement first draft

--- world_synthesis_fe/src/pages/Homepage/components/MissionStatement/index.tsx
+++ world_synthesis_fe/src/pages/Homepage/components/MissionStatement/index.tsx
@@ -7,6 +7,12 @@ export interface MissionStatementProps {
   handleClose: () => void;
 }
 
+const content: string = `
+We strive to facilitate collaboration and creativity in the fields of art, science, 
+philosophy, and more. We operate on the assumption that we are probably missing something that you have.
+It is with this humble understanding that we seek to fill the gap between the knowledge of the world and the knowledge of the individual. 
+`;
+
 const MissionStatement = (props: MissionStatementProps) => {
   const { open, handleClose } = props;
 
@@ -17,7 +23,7 @@ const MissionStatement = (props: MissionStatementProps) => {
       title="Mission Statement"
     >
       <Typography variant="body1">
-        Placeholder content for Mission Statment.
+        {content}
       </Typography>
     </GSDialogText>
   );
